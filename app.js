2f15dd4051c236bc4b147dacf82cddea908c3a30
Jubin Joy <jubinjoy432@gmail.com>
2023-02-10 10:21:26 +0530

    Fest Concluded.So necessary changes are made

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 
     //I'm adding this section so I don't have to keep updating this pen every year :-)
     //remove this if you don't need it
-    let today = new Date(),
+   let today = new Date(),
         dd = String(today.getDate()).padStart(2, "0"),
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
@@ -15,9 +15,9 @@
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
-    if (today > birthday) {
-        birthday = dayMonth + nextYear;
-    }
+   // if (today > birthday) {
+   //     birthday = dayMonth + nextYear;         /**************undo this comment when you need the countdown to start***************/
+  //  }
     //end
     
     const countDown = new Date(birthday).getTime(),
@@ -30,7 +30,7 @@
             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
           //do something later when date is reached
         if (distance < 0) {
-            document.getElementById("headline").innerText = "It's my TECHNE day!";
+            document.getElementById("headline").innerText = "The festival has come to a close, but we look forward to reuniting next year!";/****change it to "It's Techne Day" */
             document.getElementById("countdown").style.display = "none";
             document.getElementById("content").style.display = "block";
             clearInterval(x);
